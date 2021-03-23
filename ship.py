a373d702635a23d2943c112c29c06655edb37305
maxwell <daniel@maxwell.com>
2021-03-24 00:47:22 +0100

    Adding the ship image and creating the ship class.

---
+++ ship.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Ship:
+    """A class to manage the ship."""
+    
+    def __init__(self, ai_game):
+        """Initialize the ship and set itsstarting position."""
+        self.screen = ai_game.screen.get_rect()
+
+        # Load the ship image and get its react.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
