5ea66e347f53bdfb4b197db889941f73489d0d50
maxwell <daniel@maxwell.com>
2021-03-27 02:04:40 +0100

    Dodanie notatki do alien_invasion.py.

--- alien_invasion.py
+++ alien_invasion.py
@@ -65,3 +65,7 @@ if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
+
+# Note:
+# Make sure you can quit by pressing Q before running the game in fullscreen
+# mode; Pygame offers no default way to quit a game while in fullscreen mode.
