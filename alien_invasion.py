c535479569d335ca87d98bd66519a44b76abd8ad
maxwell <daniel@maxwell.com>
2021-03-29 16:22:20 +0200

    Creating a row of aliens.

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,9 +80,19 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
-        # Make an alien.
+        # Create an alien and find the number of aliens in a row.
+        # Spacing between each alien is equal to one alien width.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Create the first row of aliens.
+        for alien_number in range(number_aliens_x):
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """Upadte images on the screen, and flip to the new screen."""
@@ -110,3 +120,7 @@ if __name__ == '__main__':
 # out and see if they work. At worst, you'll have a sreen that's overcrowded
 # width aliens or has too few aliens. You can then revise your calculations
 # based on what you see on the screen.
+
+# Note:
+# Depending on the screen width you've chosen, the alignement of the first row
+# of aliens might look slightly different on your system.
