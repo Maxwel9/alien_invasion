2664996cc62e0d7af27b499e2f2cee2fb65d1f12
maxwell <daniel@maxwell.com>
2021-04-01 00:56:04 +0200

    Updating the score as aliens are shot down.

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ class Settings:
         self.fleet_drop_speed = 8
 
         # How quickly the game speeds up
-        self.speedup_scale = 10
+        self.speedup_scale = 1.1
 
         self.initialize_dynamic_settings()
 
@@ -34,6 +34,9 @@ class Settings:
         
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+        # Scoring
+        self.alien_points = 50
 
     def increase_speed(self):
         """Increase speed settings."""
